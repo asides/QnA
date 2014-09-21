93cf5b2ea3871e668c2f4b534b8fa46ee2c8d1d4
yaroslav.str <yaroslav.str@gmail.com>
2014-09-21 21:35:02 +0300

    write red User sign_in test

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
-feature "Вход пользователя", %q{
+feature 'Вход пользователя', %q{
   Для того что бы завать вопросы
   Как пользователь
   Я хочу войти
 } do
-  scenario "Зарегистрированный пользователь пытается войти" do
+  scenario 'Зарегистрированный пользователь пытается войти' do
     User.create!(email: 'user@test.com', password: '12345678')
 
-    visit "/sign_in"
-    fill_in "Email", with: "user@test.com"
-    fill_in "Password", with: "12345678"
+    visit '/sign_in'
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
     click_on 'Sign in'
+
+    expect(page).to have_content 'Вы успешно вошли. Добро пожаловать!'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Не зарегистрованный пользователь пытается войти' do
+    visit '/sign_in'
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Sign in'
+
+    expect(page).to have_content 'Неверный email или пароль'
+    expect(current_path).to eq '/sing_in'
   end
 end
