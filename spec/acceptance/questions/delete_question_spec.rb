44a47f01e82f3481ef36ecfc0379209048736632
yaroslav.str <yaroslav.str@gmail.com>
2014-09-30 20:45:56 +0300

    delete_question_spec green

--- spec/acceptance/questions/delete_question_spec.rb
+++ spec/acceptance/questions/delete_question_spec.rb
@@ -1,7 +1,27 @@
 require "rails_helper"
 
 feature "Автор вопроса может удалить свой вопрос" do
-  scenario "Автор вопроса удаляет свой вопрос"
-  scenario "Аутентифицированный пользователь не может удалять вопросы другх пользователей"
-  scenario "Гость не может удалять вопросы"
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+  given(:question) { create(:question, user: user) }
+
+  scenario "Автор вопроса удаляет свой вопрос" do
+    sign_in user
+    
+    visit question_path(question)
+    click_on 'Удалить вопрос'
+
+    expect(current_path).to eq questions_path
+    expect(page).to have_content "Question with id:#{question.id} deleted!"
+  end
+  scenario "Аутентифицированный пользователь не может удалять вопросы другх пользователей" do
+    sign_in user2   
+    visit question_path(question)  
+    expect(page).to_not have_link 'Удалить вопрос'
+  end
+
+  scenario "Гость не может удалять вопросы" do
+    visit question_path(question)  
+    expect(page).to_not have_link 'Удалить вопрос'
+  end
 end
