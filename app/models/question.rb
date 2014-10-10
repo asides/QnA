191752e9bb4956de8c625cece5e9b301842561dd
yaroslav.str <yaroslav.str@gmail.com>
2014-10-10 23:12:25 +0300

    Attachment model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
 
   belongs_to :user
   has_many :answers, dependent: :destroy
-
+  has_many :attachments, dependent: :destroy
   validates :title, length: { maximum: 255 }, presence: true
   validates :body, length: { maximum: 1000 }, presence: true
   validates :user, presence: true
