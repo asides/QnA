f3320726a8f0a44f3126cf6c250ff202c7ea669e
yaroslav.str <yaroslav.str@gmail.com>
2014-11-09 13:46:57 +0200

    add relation for question to tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,13 @@
 class Question < ActiveRecord::Base
 
   belongs_to :user
+
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachmentable, dependent: :destroy
   has_many :comments, as: :commentable
+
+  has_many :taggings
+  has_many :tags, through: :taggings
 
   validates :title, length: { maximum: 300 }, presence: true
   validates :body, length: { maximum: 10000 }, presence: true
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -15,4 +15,6 @@ RSpec.describe Question, type: :model do
 
   it { should accept_nested_attributes_for :attachments }
 
+  it { should have_many :taggings }
+  it { should have_many :tags }
 end
